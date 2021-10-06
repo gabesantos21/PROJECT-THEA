a0b0bbcd75cc3d6ea5b12d2b7b23192e59198c0a
Gabe Santos <gabesantos726@gmail.com>
2021-10-06 17:03:27 +0800

    MODIFIED NAVBAR
    Modified Navbar

--- Pages/UserPage/NavBar/Navbar.php
+++ Pages/UserPage/NavBar/Navbar.php
@@ -9,7 +9,8 @@
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Alata&display=swap" rel="stylesheet">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <link rel="stylesheet" href="../../../Css/styles.css">
+    <link rel="stylesheet" href="../../../Css/styles.css?v=<?php echo time(); ?>">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <title></title>
   </head>
   <body>
@@ -18,76 +19,239 @@
       // if true loads registered user navbar else guest navbar
       $_SESSION['userLogged'] = "true";
 
-      $dropdown_user = "You";
-      $dropdown_user_login = "Log In";
-      $dropdown_user_signup = "Sign Up";
+      $dropdown_user = "User"; 
+      $dropdown_greet_user = "Hi, " . $dropdown_user;
+      $dropdown_user_login = "Login";
+      $dropdown_user_signup = "Register";
+      $dropdown_user_logout = "Logout";
       $nav_link_home = "Home";
       $nav_link_store = "Store";
+      $nav_link_contact = "Contact Us";
+      $nav_link_about = "About Us";
+      $nav_search_placeholder = "Search";
+      $modal_login_gmail = "Login with Gmail";
+      $modal_login_facebook = "Login with Facebook";      
+      $modal_register_gmail = "Register with Gmail";
+      $modal_register_facebook = "Register with Facebook";
+      $modal_email = "Email";
+      $modal_password = "Password";
+      $modal_submit = "Submit";
+      $modal_close = "Close";
+      $modal_givenName = "Given Name";
+      $modal_surname = "Surname";
+      $modal_CPassword = "Confirm Password";
+      $modal_number = "Phone Number";
+      $modal_save = "Save Changes";
+      $modal_user = "User";
+      $modal_address = "Address";
+      $modal_city = "City";
+      $modal_barangay = "Barangay";
+      $modal_zip = "ZIP";
       ?>
  
     <nav>
-    <div class="burger">
-          <div class="line1"></div>
-          <div class="line2"></div>
-          <div class="line3"></div>
-    </div>
-    <ul class="nav-links">
-    <form action="/" method="GET" class="search-bar" style="margin: 30px 0 0 0;">
-        <input type="search" placeholder="Search" class="search-field-responsive"/>
-        <button type="submit" class="search-button">
-          <img src="../../../Assets/img/icons/search2.svg">
-        </button>
-    </form>
-        <li><a href="">About Us</a></li>
-        <li><a href="">Contact Us</a></li>
-        <li><a href="" class="nav-home-link"><img src="../../../Assets/img/icons/storefront.svg" alt="">&nbsp;Shop</a></li>
-      </ul>
-      <ul class="search">
-      <form action="/" method="GET" class="search-bar">
-        <input type="search" placeholder="Search" name="search-item" class="search-field"/>
-        <button type="submit" class="search-button">
-          <img src="../../../Assets/img/icons/search.svg">
-        </button>
-      </form>
-      </ul>
-      <div class="logo">
-        <div style="position: relative" class="nav-home">
-            <li><a href="" class="nav-home-link">Home <img src="../../../Assets/img/icons/dropdown.svg" alt=""> |</a></li>
-            <div class="dropdown-content">
-                <a href="#" class="dropdown-items" style="color: #c8b8b2">About Us</a>
-                <a href="#" class="dropdown-items" style="color: #c8b8b2">Contact Us</a>
-             </div>
-        </div>
-        <img src="../../../Assets/img/logo/text-logo.png" alt="Heavenly Baked By Ningning">
-        <li><a href="">| Store</a></li>
+      <div class="burger">
+            <div class="line1"></div>
+            <div class="line2"></div>
+            <div class="line3"></div>
       </div>
-      <ul class="user-nav-links" style="display: relative">
-        <li><a href=""><img src="../../../Assets/img/icons/cart.svg" alt=""></a></li>
-        <li  class="nav-user"><a href=""><img src="../../../Assets/img/icons/user.svg" alt=""></a>
-        <div class="dropdown-user-control">
-            <?php 
-            if (@$_SESSION['userLogged'] == "true") {
-                ?>
-                <h3 class="dropdown-items">Hi User</h3>
-                <form action="/" method="GET">
-                    <input type="submit" style="color: #c8b8b2;cursor: pointer;" class="dropdown-items" name="logout" value="Logout">
-                </form
-                <?php
-            }
-            else{?>
-                <form action="/" method="GET">
-                    <input type="submit" style="color: #c8b8b2;cursor: pointer;" class="dropdown-items" name="login" value="Login">
+      <ul class="nav-links">
+        <form action="/" method="GET" class="search-bar" style="margin: 30px 0;">
+            <input type="search" placeholder="<?php echo $nav_search_placeholder?>" class="search-field-responsive"/>
+            <button type="submit" class="search-button">
+              <img src="../../../Assets/img/icons/search2.svg">
+            </button>
+        </form>
+          <li><a href="#AboutUs" class="home-link-about"><?php echo $nav_link_about?></a></li>
+          <li><a href="#ContactUs" class="home-link-contact"><?php echo $nav_link_contact?></a></li>
+          <li><a href="" class="nav-home-link"><img src="../../../Assets/img/icons/storefront.svg" alt="">&nbsp;<?php echo $nav_link_store?></a></li>
+      </ul>
+
+        <ul class="search">
+        <form action="/" method="GET" class="search-bar">
+          <input type="search" placeholder="<?php echo $nav_search_placeholder?>" name="search-item" class="search-field"/>
+          <button type="submit" class="search-button">
+            <img src="../../../Assets/img/icons/search.svg">
+          </button>
+        </form>
+        </ul>
+
+        <div class="logo">
+          <div style="position: relative" class="nav-home">
+              <li><a href="" class="nav-home-link"><?php echo $nav_link_home ?><img src="../../../Assets/img/icons/dropdown.svg" alt=""> |</a></li>
+              <div class="dropdown-content">
+                  <a href="#AboutUs" class="dropdown-items" style="letter-spacing: 1px;"><?php echo $nav_link_about?></a>
+                  <a href="#ContactUs" class="dropdown-items" style="letter-spacing: 1px;"><?php echo $nav_link_contact?></a>
+              </div>
+          </div>
+          <img src="../../../Assets/img/logo/text-logo.png" alt="Heavenly Baked By Ningning">
+          <li><a href="">| <?php echo $nav_link_store?></a></li>
+        </div>
+
+        <ul class="user-nav-links" style="display: relative">
+          <li>
+            <a href="">
+              <img src="../../../Assets/img/icons/cart.svg" alt="">
+            </a>
+          </li>
+          <li class="nav-user">
+              <img src="../../../Assets/img/icons/user.svg" alt="">
+            <div class="dropdown-user-control">
+              <?php if (@$_SESSION['userLogged'] == "true") { ?>
+                <a href=""class="dropdown-items" style="letter-spacing: 0px" data-toggle="modal" data-target=".user-modal-container"><?php echo $dropdown_greet_user?></a>   
+                <form action="">
+                <a href=""class="dropdown-items" style="letter-spacing: 0px" ><?php echo $dropdown_user_logout?></a>
                 </form>
-                <form action="/" method="GET">
-                    <input type="submit" style="color: #c8b8b2;cursor: pointer;" class="dropdown-items" name="register" value="Register">
-                </form>
-                <?php } ?>
-             </div>  
-             </li>
-    </ul>
+                    <?php
+                }
+                    else{?>
+                <a href=""class="dropdown-items" style="letter-spacing: 0px" data-toggle="modal" data-target=".login-modal-container"><?php echo $dropdown_user_login?></a>
+                <a href=""class="dropdown-items" style="letter-spacing: 0px" data-toggle="modal" data-target=".register-modal-container"><?php echo $dropdown_user_signup?></a>
+          <?php } ?>
+            </div>  
+          </li>
+        </ul>
     </nav>
 
+     <!-- Login Modal -->
+     <div class="modal fade bd-example-modal login-modal-container" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+      <div class="modal-dialog modal modal-dialog-centered">
+        <div class="modal-content modal-container">
+          <div class="modal-header modal-header-center">
+            <h5 class="modal-title modal-title-size"><?php echo $dropdown_user_login ?></h5>
+            <button type="button" class="gmail-login"><?php echo $modal_login_gmail ?></button>
+            <button type="button" class="facebook-login"><?php echo $modal_login_facebook ?></button>
+          </div>
+          <div class="modal-body">
+            <form action="" method="post">
+              <div class="user-form">
+                <div class="form-group">
+                  <label for="inputEmail"><?php echo $modal_email ?></label>
+                  <input type="email" class="form-control" id="inputEmail" name="email" placeholder="" required>
+                </div>
+                <div class="form-group">
+                  <label for="inputPassword"><?php echo $modal_password ?></label>
+                  <input type="password" class="form-control" id="inputPassword" name="password" placeholder="" required>
+                </div>
+              </div>
+              <div class="modal-footer">
+                <button type="submit" class="submit-btn" name="submit"><?php echo  $modal_submit ?></button>
+                <button type="button" class="submit-btn" style="color: #433534; background: #fbfdfe;" data-dismiss="modal"><?php echo $modal_close ?></button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+
+      <!-- Register Modal -->
+      <div class="modal fade bd-example-modal register-modal-container" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+      <div class="modal-dialog modal modal-dialog-centered">
+        <div class="modal-content modal-container">
+          <div class="modal-header modal-header-center">
+            <h5 class="modal-title modal-title-size"><?php echo $dropdown_user_signup ?></h5>
+            <button type="button" class="gmail-login"><?php echo $modal_register_gmail ?></button>
+            <button type="button" class="facebook-login"><?php echo $modal_register_facebook ?></button>
+          </div>
+          <div class="modal-body">
+            <form action="" method="post">
+              <div class="user-form">
+                <div class="form-row">
+                  <div class="form-group col-md-6">
+                    <label for="inputName"><?php echo $modal_givenName ?></label>
+                    <input type="text" class="form-control" id="inputName" name="name" placeholder="" required>
+                  </div>
+                  <div class="form-group col-md-6">
+                    <label for="inputSurname"><?php echo $modal_surname ?></label>
+                    <input type="text" class="form-control" id="inputSurname" name="surname" placeholder="" required>
+                  </div>
+                </div>
+                <div class="form-group">
+                  <label for="inputNumber"><?php echo $modal_number ?></label>
+                  <input type="tel" class="form-control" id="inputNumber" name="number" placeholder="" required>
+                </div>
+                <div class="form-group">
+                  <label for="inputEmail"><?php echo $modal_email ?></label>
+                  <input type="email" class="form-control" id="inputEmail" name="email" placeholder="" required>
+                </div>
+                <div class="form-group">
+                  <label for="inputPassword"><?php echo $modal_password ?></label>
+                  <input type="password" class="form-control" id="inputPassword" name="password" placeholder="" required>
+                </div>
+                <div class="form-group">
+                  <label for="inputConfirmPassword"><?php echo $modal_CPassword ?></label>
+                  <input type="password" class="form-control" id="inputConfirmPassword" name="confirmPassword" placeholder="" required>
+                </div>
+              </div>
+              <div class="modal-footer">
+                <button type="submit" class="submit-btn" name="submit"><?php echo $modal_submit ?></button>
+                <button type="button" class="submit-btn" style="color: #433534; background: #fbfdfe;" data-dismiss="modal"><?php echo $modal_close?></button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- User Modal -->
+    <div class="modal fade bd-example-modal user-modal-container" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+      <div class="modal-dialog modal modal-dialog-centered">
+        <div class="modal-content modal-container">
+          <div class="modal-header modal-header-center">
+            <h5 class="modal-title modal-title-size"><?php echo $modal_user ?></h5>
+          </div>
+          <div class="modal-body">
+            <form action="" method="post">
+              <div class="user-form">
+                <div class="form-row">
+                  <div class="form-group col-md-6">
+                    <label for="inputName"><?php echo $modal_givenName ?></label>
+                    <input type="text" class="form-control" id="inputName" name="name" placeholder="" required>
+                  </div>
+                  <div class="form-group col-md-6">
+                    <label for="inputSurname"><?php echo $modal_surname ?></label>
+                    <input type="text" class="form-control" id="inputSurname" name="surname" placeholder="" required>
+                  </div>
+                </div>
+                <div class="form-group">
+                  <label for="inputEmail"><?php echo $modal_email ?></label>
+                  <input type="email" class="form-control" id="inputEmail" name="email" placeholder="" required>
+                </div>
+                <div class="form-group">
+                  <label for="inputPassword"><?php echo $modal_password ?></label>
+                  <input type="password" class="form-control" id="inputPassword" name="password" placeholder="" required>
+                </div>
+                <div class="form-group">
+                  <label for="inputAddress"><?php echo $modal_address ?></label>
+                  <input type="text" class="form-control" id="inputAddress" name="address" placeholder="" required>
+                </div>
+                <div class="form-group">
+                  <label for="inputCity"><?php echo $modal_city ?></label>
+                  <input type="text" class="form-control" id="inputCity" name="city" placeholder="" required>
+                </div>
+                <div class="form-group">
+                  <label for="inputBarangay"><?php echo $modal_barangay ?></label>
+                  <input type="text" class="form-control" id="inputAddress" name="barangay" placeholder="" required>
+                </div>
+                <div class="form-group">
+                  <label for="inputZIP"><?php echo $modal_zip ?></label>
+                  <input type="text" class="form-control" id="inputZIP" name="ZIP" placeholder="" required>
+                </div>
+              </div>
+              <div class="modal-footer">
+                <button type="submit" class="submit-btn" name="submit"><?php echo $modal_save ?></button>
+                <button type="button" class="submit-btn" style="color: #433534; background: #fbfdfe;" data-dismiss="modal"><?php echo $modal_close ?></button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+    
    
     <script src="../../../Js/app.js"></script>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
   </body>
 </html>
