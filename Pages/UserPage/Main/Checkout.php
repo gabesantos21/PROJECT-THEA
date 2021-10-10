2980c56b7fd3c4f2dd023b7cd6dd9473a857a259
Luis-Panaligan <201901428@iacademy.edu.ph>
2021-10-10 17:43:25 +0800

    MODIFIED FILES

--- Pages/UserPage/Main/Checkout.php
+++ Pages/UserPage/Main/Checkout.php
@@ -2,21 +2,21 @@
 <!DOCTYPE html>
 <html>
 <head>
-
 	<link rel="preconnect" href="https://fonts.googleapis.com">
 	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 	<link href="https://fonts.googleapis.com/css2?family=Alata&display=swap" rel="stylesheet">
 	<script src="https://kit.fontawesome.com/37cafce672.js" crossorigin="anonymous"></script>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>test</title>
-	<link rel="stylesheet" href="../../../Css/styles-for-checkout.css">
+	<title>Checkout</title>
+	<link rel="stylesheet" href="../../../Css/styles-for-checkout.css?v=<?php echo time(); ?>">
+	<link rel="stylesheet" href="../../../Css/styles.css?v=<?php echo time(); ?>">
 	<style>
-		@import url('https://fonts.googleapis.com/css2?family=Alata&display=swap');  
+		@import url('https://fonts.googleapis.com/css2?family=Alata&display=swap'); 
 	</style>
 </head>
 <body>
-<div class="page-header"><div class="header-text">CHECKOUT</div></div>
+<div class="page-header"><div class="header-text">CHECKOUT</div></div>	
 	<form>
 		<div class="container-main">		
     		<div class="flex-box-1">
