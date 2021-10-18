14736dccee946261727b38bbf6a3d5f778d22236
Luis-Panaligan <201901428@iacademy.edu.ph>
2021-10-18 21:42:05 +0800

    MODIFIED FILES

--- Pages/AdminPage/Main/OrderStatusPage.php
+++ Pages/AdminPage/Main/OrderStatusPage.php
@@ -4,54 +4,163 @@
         <title>ORDERS</title>
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
-	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-	<link href="https://fonts.googleapis.com/css2?family=Alata&display=swap" rel="stylesheet">
-	<script src="https://kit.fontawesome.com/37cafce672.js" crossorigin="anonymous"></script>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<meta http-equiv="X-UA-Compatible" content="ie=edge" />
-	<title>Checkout</title>
-	<link rel="stylesheet" href="../../../Css/styles-for-checkout.css?v=<?php echo time(); ?>">
-	<link rel="stylesheet" href="../../../Css/styles.css?v=<?php echo time(); ?>">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Alata&display=swap" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/37cafce672.js" crossorigin="anonymous"></script>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <title>Checkout</title>
+    <link rel="stylesheet" href="../../../Css/styles-for-checkout.css?v=<?php echo time(); ?>">
+    <link rel="stylesheet" href="../../../Css/styles.css?v=<?php echo time(); ?>">
     </head>
 
     <body>
 
-
     <?php 
-	if (isset($_GET["action"])) {
+    if (isset($_GET["action"])) {
         if ($_GET["action"] == "delete") {
           echo "<script>
-		  $(document).ready(function() {
+          $(document).ready(function() {
           $('#success-remove-alert')
           .fadeTo(2000, 500)
           .slideUp(500, function () {
             $('#success-remove-alert').slideUp(500);
           });
-		});
+        });
                 </script>";
         }
       }
-	?>
+    ?>
 
         <div class = "header-border">
-	        <div class="page-banner"><div class="banner-text">ORDERS</div></div>
+            <div class="page-banner"><div class="banner-text">ORDERS</div></div>
         </div>
 
+        <div class="order-list-nav">
+            <a href="OrderStatusPage.php?list=completed">Completed Orders</a>
+            <a href="OrderStatusPage.php">Pending Orders</a>
+            <a href="OrderStatusPage.php?list=declined">Cancelled Orders</a>
+        </div>
 
-        <div class="orderPage-table-container">
+        <!-- TODO for backend - automate the rows -->
+
+        <!-- Completed Order Table -->
+        <?php if(isset($_GET["list"])){
+            if ($_GET["list"] == "completed"){
+        ?>
+         <div class="orderPage-table-container">
+            <h4>Completed Orders</h4>
             <table class="orderPage-table">
                 <th style="width:15%">Order ID</th>
                 <th style="width:15%">Customer ID</th>
-                <th style="width:40%">Order Date</th>
-                <th style="width:15%">Total Amount</th>
-                <th style="width:15%">Order Status</th>
+                <th style="width:35%">Order Date</th>
+                <th style="width:10%">Total Amount</th>
+                <th style="width:10%">Order Status</th>
+                <th style="width:15%">Update Status</th>
                     <tr>
                         <td data-label="Order ID">1</td>
                         <td data-label="Customer ID">1</td>
                         <td data-label="Order Date">06:06:30 PM, 3rd July 2021</td>
                         <td data-label="Total Amount">1</td>
-                        <td data-label="Order Status">Confirmed</td>
+                        <td data-label="Order Status">Completed</td>
+                        <td data-label="Update Status">
+                            <form><input type="radio" id="revert" name="orderComplete" value="revert"/>Revert</td></form>
+                    </tr>
+                    <tr>
+                        <td data-label="Order ID">2</td>
+                        <td data-label="Customer ID">2</td>
+                        <td data-label="Order Date">06:06:30 PM, 3rd July 2021</td>
+                        <td data-label="Total Amount">2</td>
+                        <td data-label="Order Status">Completed</td>
+                        <td data-label="Update Status">
+                            <form><input type="radio" id="revert" name="orderComplete" value="revert"/>Revert</td></form>
+                    </tr>
+                    <tr>
+                        <td data-label="Order ID">3</td>
+                        <td data-label="Customer ID">3</td>
+                        <td data-label="Order Date">06:06:30 PM, 3rd July 2021</td>
+                        <td data-label="Total Amount">3</td>
+                        <td data-label="Order Status">Completed</td>
+                        <td data-label="Update Status">
+                            <form><input type="radio" id="revert" name="orderComplete" value="revert"/>Revert</td></form>
+                    </tr>
+            </table>
+            <div class="orderPage-table-foot">
+                <div style="float:right">Apply Change:<input type="submit" value="Apply" class="orderPage-table-input"></div>
+                <div style="float:right">Reset Buttons:<input type="button" value="Reset" class="orderPage-table-input" onclick="clearSelection('orderComplete');"></div>     
+            </div>
+        </div>
+        <?php
+            } else if ($_GET["list"] == "declined"){
+        ?>
+            <!-- Declined Orders Table -->
+        <div class="orderPage-table-container">
+        <h4>Cancelled Orders</h4>
+        <table class="orderPage-table">
+                <th style="width:15%">Order ID</th>
+                <th style="width:15%">Customer ID</th>
+                <th style="width:35%">Order Date</th>
+                <th style="width:10%">Total Amount</th>
+                <th style="width:10%">Order Status</th>
+                <th style="width:15%">Update Status</th>
+                    <tr>
+                        <td data-label="Order ID">1</td>
+                        <td data-label="Customer ID">1</td>
+                        <td data-label="Order Date">06:06:30 PM, 3rd July 2021</td>
+                        <td data-label="Total Amount">1</td>
+                        <td data-label="Order Status">Cancelled</td>
+                        <td data-label="Update Status">
+                            <form><input type="radio" id="revert" name="orderDecline" value="revert"/>Revert</td></form>
+                    </tr>
+                    <tr>
+                        <td data-label="Order ID">2</td>
+                        <td data-label="Customer ID">2</td>
+                        <td data-label="Order Date">06:06:30 PM, 3rd July 2021</td>
+                        <td data-label="Total Amount">2</td>
+                        <td data-label="Order Status">Cancelled</td>
+                        <td data-label="Update Status">
+                            <form><input type="radio" id="revert" name="orderDecline" value="revert"/>Revert</td></form>
+                    </tr>
+                    <tr>
+                        <td data-label="Order ID">3</td>
+                        <td data-label="Customer ID">3</td>
+                        <td data-label="Order Date">06:06:30 PM, 3rd July 2021</td>
+                        <td data-label="Total Amount">3</td>
+                        <td data-label="Order Status">Cancelled</td>
+                        <td data-label="Update Status">
+                            <form><input type="radio" id="revert" name="orderDecline" value="revert"/>Revert</td></form>
+                    </tr>
+            </table>
+            <div class="orderPage-table-foot">
+                <div style="float:right">Apply Change:<input type="submit" value="Apply" class="orderPage-table-input"></div>
+                <div style="float:right">Reset Buttons:<input type="button" value="Reset" class="orderPage-table-input" onclick="clearSelection('orderDecline');"></div>     
+            </div>
+        </div>
+        <?php }
+            }else{?>
+            <!-- Pending Orders Table -->
+        <div class="orderPage-table-container">
+        <h4>Pending Orders</h4>
+        <table class="orderPage-table">
+            <th style="width:15%">Order ID</th>
+                <th style="width:15%">Customer ID</th>
+                <th style="width:35%">Order Date</th>
+                <th style="width:10%">Total Amount</th>
+                <th style="width:10%">Order Status</th>
+                <th style="width:15%">Update Status</th>
+                    <tr>
+                        <td data-label="Order ID">1</td>
+                        <td data-label="Customer ID">1</td>
+                        <td data-label="Order Date">06:06:30 PM, 3rd July 2021</td>
+                        <td data-label="Total Amount">1</td>
+                        <td data-label="Order Status">Pending</td>
+                        <td data-label="Update Status">
+                            <form>
+                            <input type="radio" id="complete" name="orderPending" value="decline"/>Complete
+                            <input type="radio" id="decline" name="orderPending" value="decline"/>Decline
+                            </form>
+                    </td>                   
                     </tr>
                     <tr>
                         <td data-label="Order ID">2</td>
@@ -59,6 +168,12 @@
                         <td data-label="Order Date">06:06:30 PM, 3rd July 2021</td>
                         <td data-label="Total Amount">2</td>
                         <td data-label="Order Status">Pending</td>
+                        <td data-label="Update Status">
+                            <form>
+                            <input type="radio" id="complete" name="orderPending" value="decline"/>Complete
+                            <input type="radio" id="decline" name="orderPending" value="decline"/>Decline
+                            </form>
+                    </td>
                     </tr>
                     <tr>
                         <td data-label="Order ID">3</td>
@@ -66,8 +181,32 @@
                         <td data-label="Order Date">06:06:30 PM, 3rd July 2021</td>
                         <td data-label="Total Amount">3</td>
                         <td data-label="Order Status">Pending</td>
+                        <td data-label="Update Status">
+                            <form>
+                            <input type="radio" id="complete" name="orderPending" value="decline"/>Complete
+                            <input type="radio" id="decline" name="orderPending" value="decline"/>Decline
+                            </form>
+                    </td>
+                    </tr>
                     </tr>
             </table>
+            <div class="orderPage-table-foot">
+                <div style="float:right">Apply Change:<input type="submit" value="Apply" class="orderPage-table-input"></div>
+                <div style="float:right">Reset Buttons:<input type="button" value="Reset" class="orderPage-table-input" onclick="clearSelection('orderPending');"></div>     
+            </div>
         </div>
+        <?php }?>
+
+
+    <!-- Resets radio buttons -->
+    <script>
+        const clearSelection = (name) => {
+            const radio = document.querySelectorAll("input[type='radio'][name='" + name + "']");
+            radio.forEach(radio => {
+                if(radio.checked === true)radio.checked = false;
+            });
+        };
+    </script>
+
     </body>
 </html>
